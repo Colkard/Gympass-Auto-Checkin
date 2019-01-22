68d8074cd3bd25cb7eafbdd97af641883f3aa42e
López P, Alberto <alberto.lopez.p@accenture.com>
2019-01-22 18:19:52 +0100

    Working login Gympass

---
+++ Config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    api_url: 'https://crossfitboxcastelldefels.aimharder.com/api',
+    login_url: 'https://www.gympass.com/es/personas/entrar',
+    iCountDaysToBook: 3,
+    iBoxID: "9283",
+    aDaysToBook: [
+        {
+            Day: 1,
+            Time: "21:00 - 22:00",
+            ClassName: "WOD"
+        },
+        {
+            Day: 2,
+            Time: "21:00 - 22:00",
+            ClassName: "Barbell Conditioning"
+        },
+        {
+            Day: 3,
+            Time: "20:30 - 21:30",
+            ClassName: "WOD"
+        },
+        {
+            Day: 4,
+            Time: "20:30 - 21:30",
+            ClassName: "Weightlifting"
+        },
+        {
+            Day: 5,
+            Time: "18:30 - 19:30",
+            ClassName: "WOD"
+        },
+    ]
+};
--- index.js
+++ index.js
@@ -1,19 +1,38 @@
-var phantom = require("phantom");
-var _ph, _page, _outObj;
+const request = require('request');
+const moment = require('moment');
+var _ = require('lodash');
+const cheerio = require('cheerio');
 
-phantom.create().then(function(ph){
-    _ph = ph;
-    return _ph.createPage();
-}).then(function(page){
-    _page = page;
-    return _page.open('https://www.gympass.com/es/personas/entrar');
-}).then(function(status){
-    console.log(status);
-    return _page.property('title')
-}).then(function(content){
-    console.log(content);
-    _page.close();
-    _ph.exit();
-}).catch(function(e){
-   console.log(e);
+var config = require('./Config');
+
+var aCookies, sCSRFToken;
+var login1 = (fnSuccess, fnError) => {
+    request(config.login_url, function (error, response, body) {
+        if (!error) {
+            sCSRFToken = cheerio.load(body)("meta[name=csrf-token]").attr("content");
+            aCookies = response.headers["set-cookie"];
+            fnSuccess && fnSuccess();
+        } else {
+            fnError && fnError();
+        }
+    });
+};
+
+var login2 = fnSuccess => {
+    request({
+        url: config.login_url,
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "cookie": aCookies.join("; ")
+        },
+        body: `utf8=%E2%9C%93&authenticity_token=${encodeURIComponent(sCSRFToken)}&source_page=main-flow&person%5Bemail%5D=${encodeURIComponent(process.env.email)}&person%5Bpassword%5D=${encodeURIComponent(process.env.pass)}&person%5Bremember_me%5D=0&person%5Bremember_me%5D=1&button=`
+    }).on('response', res => {
+        console.log(res.headers["set-cookie"])
+        fnSuccess && fnSuccess();
+    })
+};
+
+login1(() => {
+    login2();
 });
